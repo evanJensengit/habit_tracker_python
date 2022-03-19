49b6500b8e16f4035f31a2e585db3aa966127d3c
Evan Jensen <jensen55@uw.edu>
2022-03-19 00:05:37 -0700

    hi

--- main.py
+++ main.py
@@ -17,18 +17,31 @@ class Week:
     def __init__(self):
         return
 
+    #increments number of week object data members based on data in lines list
     def processWeek(self, lines):
         
         for line in lines: 
-            words = line.split(":")
-            if len(words) > 1:
-                print(words[1],end='')
-                
-                if len(words) == 2:
-                   YornAndType = words[1].split(" ")
+            
+            words = line.split(":") #get description of item to be tracked and value of that item
+
+            if len(words) > 1: #there is something in the line
+                descript = words[0]
+                YornAndType = []
+
+                if len(words) > 2: 
+                   YornAndType = words[1].split(' ') #get the y and other data 
                 else:
-                    YornAndType = words[1]
-                if words[0] == "Wrkt" and YornAndType[1]=="y":
+                    YornAndType.append(words[1]) #no type 
+
+                # for word in words:
+                #     print(word,end='')
+                count = 0
+                for yorn in YornAndType:
+                    
+                    print(count, " ", yorn)
+                    count +=1
+                #print("hi" + YornAndType[0],end='')
+                if descript == workout and words[1][1]=="y":
                     self.workout += 1
 
             
@@ -51,4 +64,5 @@ with open('pci.txt') as f:
     lines = f.readlines()
 week = Week()
 week.processWeek(lines)
+print()
 week.printWeek()
